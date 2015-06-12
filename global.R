7c21236466ea4fa59f8a1aa828e3cea9dfe76ec2
Guillaume Lobet <guillaume.lobet@gmail.com>
2015-06-12 11:46:10 +0200

    update the ibrayr requirements

--- global.R
+++ global.R
@@ -3,5 +3,12 @@
 
 
 # Global libraries
-  library(plyr)
-  library(visNetwork)
+  packages <- c("visNetwork")
+
+  for(p in packages){
+    if (!require(p,character.only = TRUE))
+    {
+      install.packages(p,dep=TRUE)
+      if(!require(p,character.only = TRUE)) stop("Package not found")
+    }
+  }
